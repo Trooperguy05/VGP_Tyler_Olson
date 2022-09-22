dfd77c691726085212ea28859585f634fc67a0de
Trooperguy05 <Tyry2005@outlook.com>
2022-09-22 09:14:54 -0700

    wrsFASDfSDFS
    SDFSDfSDFDZSV

--- Prototype 3/Prototype 3/Assets/Scripts/playerController.cs
+++ Prototype 3/Prototype 3/Assets/Scripts/playerController.cs
@@ -10,31 +10,44 @@ public class playerController : MonoBehaviour
     public bool isGrounded;
     public bool gameOver = false;
     private Animator playerAnimator;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip collisionSound;
+    private AudioSource playerAS;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         playerAnimator = gameObject.GetComponent<Animator>();
+        playerAS = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !gameOver) {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
             playerAnimator.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
     }
     
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "Ground") {
             isGrounded = true;
+            dirtParticle.Play();
         }
         else if (collision.gameObject.tag == "Obstruction") {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAS.PlayOneShot(collisionSound, 1.0f);
         }
     }
 }
